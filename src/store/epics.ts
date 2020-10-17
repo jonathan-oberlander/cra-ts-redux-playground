a4e971b1cb52a2ed2e8b0e96d60cef97bc09ef57
jonathan-oberlander <j.oberlander@novafutur.com>
2020-10-17 20:15:51 +0100

    more type

--- src/store/epics.ts
+++ src/store/epics.ts
@@ -1,32 +1,33 @@
 import { combineEpics, ofType, Epic } from 'redux-observable';
 import { delay, mapTo, catchError, switchMap,filter, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
-import { from, merge } from 'rxjs';
+import { from, merge, Observable } from 'rxjs';
 
 import { RootActionsTypes as types, TActions, AppState } from "./types";
 import { errorAction, fetchUserFulfilled, increaseValue, pong } from './actions';
 
-export const pingEpic: Epic<TActions, TActions, AppState> = (action$) => action$.pipe(
+type AppEpic = Epic<TActions, TActions, AppState>
+
+export const pingEpic: AppEpic = (action$) => action$.pipe(
   ofType(types.PING),
   delay(1000),
   mapTo(pong())
 );
 
-export const increaseIfUnderZeroEpic: Epic<TActions, TActions, AppState> = (action$, state$) => action$.pipe(
+export const increaseIfUnderZeroEpic: AppEpic = (action$, state$) => action$.pipe(
   ofType(types.INCREASE_IF_UNDER_ZERO),
   filter(() =>  state$.value.value < 10),
   mapTo(increaseValue())
 );
 
-
-export const callOnInputEpic: Epic<TActions, TActions, AppState> = (action$) => action$.pipe(
+// We have to narrow down the type
+// Using 'in' allows typescript to infer the correct type from the union type, 
+// Try to avoid explicit type casting 
+export const callOnInputEpic: AppEpic = (action$) => action$.pipe(
   ofType(types.CALL_ON_INPUT),
   debounceTime(400),
   distinctUntilChanged(),
   switchMap((action) => merge(
-    // We have to narrow down the type
-    // Using 'in' allows typescript to infer the correct type from the union type, 
-    // Try to avoid explicit type casting 
     ajax.getJSON(`https://api.github.com/users/${'name' in action && action.name}`).pipe(
       map(response => fetchUserFulfilled(response)),
       catchError((error) => from([errorAction(error)]))
@@ -45,7 +46,7 @@ const epics = [
   callOnInputEpic,
 ]
 
-export const rootEpic: Epic = (action$, state$, dependencies) =>
+export const rootEpic: Epic = (action$, state$, dependencies): Observable<TActions> =>
   combineEpics(...epics)(action$, state$, dependencies).pipe(
     catchError((error, source) => {
       console.error(error)
